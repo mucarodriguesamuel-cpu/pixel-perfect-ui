ffa6ce4ca52b10b02f8f462374ebbbc051b06880
mucarodriguesamuel-cpu <mucarodriguesamuel@gmail.com>
2026-09-12 09:22:58 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,10 +9,8 @@ import estudioImg from "@/assets/portfolio-6.jpg";
 import p1 from "@/assets/portfolio-1.jpg";
 import p2 from "@/assets/portfolio-2.jpg";
 import p3 from "@/assets/portfolio-3.jpg";
-import p4 from "@/assets/portfolio-4.jpg";
 import p5 from "@/assets/portfolio-5.jpg";
 import p6 from "@/assets/portfolio-6.jpg";
-
 export const Route = createFileRoute("/")({
   head: () => ({
     meta: [
